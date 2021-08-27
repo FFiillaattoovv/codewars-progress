e449c49823a37f2ead908a4876e8af51f251cb97
FFiillaattoovv <admirator2015@yandex.ru>
2021-08-27 11:42:10 +0300

    Add solution

---
+++ 8-kyu/Exclamation marks series #4/index.test.ts
@@ -0,0 +1,8 @@
+import { remove } from './index';
+
+test('should remove all exclamation marks from sentence but ensure a exclamation mark at the end of string', () => {
+    expect(remove('Hi!')).toBe('Hi!');
+    expect(remove('Hi')).toBe('Hi!');
+    expect(remove('Hi! Hi!')).toBe('Hi Hi!');
+    expect(remove('!Hi')).toBe('Hi!');
+})
