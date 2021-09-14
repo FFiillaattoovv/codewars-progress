54faab4a0c03db6c90c9b4c03e44231b02632ec4
FFiillaattoovv <admirator2015@yandex.ru>
2021-09-14 11:11:04 +0300

    Revert "Comment solution"
    This reverts commit 5eb02373b216e359a7fdbcfd9f2ea5bf148ce556.

--- 8-kyu/ Is integer safe to use/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { SafeInteger } from './index';
-
-test('If the number is safe must return the true otherwise false', () => {
-    expect(SafeInteger(9007199254740992)).toBe(false);
-    expect(SafeInteger(9007199254740990)).toBe(true);
-    expect(SafeInteger(-90)).toBe(true);
-})
