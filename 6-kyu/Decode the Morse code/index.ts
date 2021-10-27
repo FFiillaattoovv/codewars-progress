3ada9f8af83d0fe02308e965910d4888a0375564
FFiillaattoovv <admirator2015@yandex.ru>
2021-10-27 10:23:33 +0300

    Add solution

---
+++ 6-kyu/Decode the Morse code/index.ts
@@ -0,0 +1,9 @@
+// export function decodeMorse(morseCode: string): string {
+//     return morseCode
+//         .split(' ')
+//         .map((symbol) => (MORSE_CODE[symbol] !== undefined ? MORSE_CODE[symbol] : '&'))
+//         .join('')
+//         .replace(/&&/g, ' ')
+//         .replace(/&/g, ' ')
+//         .trim();
+// }
